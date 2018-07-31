e78afdf1830636847bab8c980ab53e9027aa815c
Harry He <zhedahht@hotmail.com>
2018-07-31 16:07:28 -0700

    Minor changes
    Minor changes

--- SocketClient/app/client.js
+++ SocketClient/app/client.js
@@ -3,42 +3,42 @@ const cp = require('child_process');
 var net = require('net');
 
 function getTimePrefix() {
-	return (new Date()).toString() + ' ';
+    return (new Date()).toString() + ' ';
 }
 
 var onConnected = function(client, connection) {
-	return function() {
-		client.write(connection + ': Hello, server! Love, Client.');
-	};
+    return function() {
+        client.write(connection + ': Hello, server! Love, Client.');
+    };
 };
 
 var onData = function(client, connection) {
-	return function(data) {
-		console.log(getTimePrefix() + 'Connection ' + connection + ' received: ' + data);
-		client.destroy();
-	};
+    return function(data) {
+        console.log(getTimePrefix() + 'Connection ' + connection + ' received: ' + data);
+        client.destroy();
+    };
 }
-	
+    
 var onClosed = function(connection) {
-	return function() {
-		console.log(getTimePrefix() + 'Connection ' + connection + ' closed');
-	};
+    return function() {
+        console.log(getTimePrefix() + 'Connection ' + connection + ' closed');
+    };
 };
 
 var onError = function(connection) {
-	return function(err) {
-		console.log(getTimePrefix() + 'Connection ' + connection + ' has error: ' + err);
-	};
+    return function(err) {
+        console.log(getTimePrefix() + 'Connection ' + connection + ' has error: ' + err);
+    };
 };
 
 var startClient = function () {
-	for (var i = 0; i < connections; ++i) {
-		var client = new net.Socket();
-		client.connect(1337, 'localhost', onConnected(client, i));
-		client.on('data', onData(client, i));
-		client.on('close', onClosed(i));
-		client.on('error', onError(i));
-	}
+    for (var i = 0; i < connections; ++i) {
+        var client = new net.Socket();
+        client.connect(1337, 'localhost', onConnected(client, i));
+        client.on('data', onData(client, i));
+        client.on('close', onClosed(i));
+        client.on('error', onError(i));
+    }
 };
 
 setInterval(startClient, 10000);
--- SocketServer/app/server.js
+++ SocketServer/app/server.js
@@ -1,19 +1,19 @@
 var net = require('net');
 
 function getTimePrefix() {
-	return (new Date()).toString() + ' ';
+    return (new Date()).toString() + ' ';
 }
 
 var server = net.createServer(function(socket) {
-	socket.on('data', function(data){
-		textChunk = data.toString('utf8');
-		console.log(getTimePrefix() + textChunk);
-		socket.write(textChunk);
-	});
-	
-	socket.on('error', function(err) {
-	   console.log(getTimePrefix() + err)
-	})
+    socket.on('data', function(data){
+        textChunk = data.toString('utf8');
+        console.log(getTimePrefix() + textChunk);
+        socket.write(textChunk);
+    });
+    
+    socket.on('error', function(err) {
+       console.log(getTimePrefix() + err)
+    })
 });
 
 server.listen(1337, 'localhost');
