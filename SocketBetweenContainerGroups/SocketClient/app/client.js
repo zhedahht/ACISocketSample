9ce5e8f79c04af30d04982f10e741d9c7856d835
Harry He <zhedahht@hotmail.com>
2018-08-02 17:35:11 -0700

    Add sample for sockets between container groups
    Add sample for sockets between container groups

---
+++ SocketBetweenContainerGroups/SocketClient/app/client.js
@@ -0,0 +1,49 @@
+var connections = 200;
+const cp = require('child_process');
+var net = require('net');
+
+var host = process.env.SOCKET_SERVER_HOST;
+var port = process.env.SOCKET_SERVER_PORT;
+
+console.log('host: ' + host + ', port: ' + port);
+
+function getTimePrefix() {
+    return (new Date()).toString() + ' ';
+}
+
+var onConnected = function(client, connection) {
+    return function() {
+        client.write(connection + ': Hello, server! Love, Client.');
+    };
+};
+
+var onData = function(client, connection) {
+    return function(data) {
+        console.log(getTimePrefix() + 'Connection ' + connection + ' received: ' + data);
+        client.destroy();
+    };
+}
+    
+var onClosed = function(connection) {
+    return function() {
+        console.log(getTimePrefix() + 'Connection ' + connection + ' closed');
+    };
+};
+
+var onError = function(connection) {
+    return function(err) {
+        console.log(getTimePrefix() + 'Connection ' + connection + ' has error: ' + err);
+    };
+};
+
+var startClient = function () {
+    for (var i = 0; i < connections; ++i) {
+        var client = new net.Socket();
+        client.connect(port, host, onConnected(client, i));
+        client.on('data', onData(client, i));
+        client.on('close', onClosed(i));
+        client.on('error', onError(i));
+    }
+};
+
+setInterval(startClient, 10000);
---
+++ SocketBetweenContainerGroups/SocketServer/app/server.js
@@ -0,0 +1,20 @@
+var net = require('net');
+
+function getTimePrefix() {
+    return (new Date()).toString() + ' ';
+}
+
+var server = net.createServer(function(socket) {
+    socket.on('data', function(data){
+        textChunk = data.toString('utf8');
+        console.log(getTimePrefix() + textChunk);
+        socket.write(textChunk);
+    });
+    
+    socket.on('error', function(err) {
+       console.log(getTimePrefix() + err)
+    })
+});
+
+server.listen(1337);
+
